d5bb9ceb2194d76ae5d839da47b8b6b302d76ca1
NonSentientExistence <david@venngren.se>
2025-10-30 15:23:53 +0100

    Added meny and loop array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string menu;
+bool running_main = true;
+
+while (running_main)
+{
+  Console.WriteLine("Choose function");
+  Console.WriteLine("A: Loop Arrays -- B: Input to Array -- C: Input to List -- D: Echo from file");
+  menu = Console.ReadLine().ToLower();
+  switch (menu)
+  {
+    case "a":
+    {
+      int[] numbers = new int[10];
+
+      for (int i = 0; i < 11; i++)
+      {
+        Console.WriteLine($"{i}");
+      }
+
+      Console.WriteLine();
+
+      for (int i = 1; i < 11; i++)
+      {
+        Console.WriteLine($"{i}");
+      }
+
+      Console.WriteLine();
+
+      for (int i = 0; i < 10; i++)
+      {
+        numbers[i] = i + 1;
+      }
+
+      Console.WriteLine();
+
+      int[] even_numbers = new int[50];
+      int array_counter = 0;
+      for (int i = 0; i < 100; i++)
+      {
+        if ((i + 1) % 2 == 0)
+        {
+          even_numbers[array_counter] = i + 1;
+          array_counter++;
+        }
+        else
+        {
+          continue;
+        }
+      }
+    }
+    break;
+
+    case "q":
+    {
+      running_main = false;
+    }
+    break;
+
+
+  }
+}      
