0e71cae08416d5ce21fef8a30709a034de1c1962
NonSentientExistence <david@venngren.se>
2025-10-31 13:08:19 +0100

    Added file create, read and write

--- Program.cs
+++ Program.cs
@@ -3,104 +3,105 @@ bool running_main = true;
 
 while (running_main)
 {
+  ConsoleClear();
   Console.WriteLine("Choose function");
   Console.WriteLine("A: Loop Arrays -- B: Input to Array -- C: Input to List -- D: Echo from file");
-  menu = Console.ReadLine().ToLower();
+  menu = Console.ReadLine()?.ToLower();
   switch (menu)
   {
-    case "a":
-    {
-      int[] numbers = new int[10];
-
-      for (int i = 0; i < 11; i++)
+    case "a": //Looping arrays
       {
-        Console.WriteLine($"{i}");
-      }
+        int[] numbers = new int[10];
 
-      Console.WriteLine();
-
-      for (int i = 1; i < 11; i++)
-      {
-        Console.WriteLine($"{i}");
-      }
-
-      Console.WriteLine();
-
-      for (int i = 0; i < 10; i++)
-      {
-        numbers[i] = i + 1;
-      }
-
-      Console.WriteLine();
-
-      Console.WriteLine("Show even number array?(y)");
-      string? show_even = Console.ReadLine().ToLower();
-
-      if (show_even == "y")
-      {
-        int[] even_numbers = new int[50];
-        int array_counter = 0;
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 11; i++)
         {
-          if ((i + 1) % 2 == 0)
+          Console.WriteLine($"{i}");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 1; i < 11; i++)
+        {
+          Console.WriteLine($"{i}");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 10; i++)
+        {
+          numbers[i] = i + 1;
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Show even number array?(y)");
+        string? show_even = Console.ReadLine()?.ToLower();
+
+        if (show_even == "y")
+        {
+          int[] even_numbers = new int[50];
+          int array_counter = 0;
+          for (int i = 0; i < 100; i++)
           {
-            even_numbers[array_counter] = i + 1;
-            array_counter++;
+            if ((i + 1) % 2 == 0)
+            {
+              even_numbers[array_counter] = i + 1;
+              array_counter++;
+            }
+            else
+            {
+              continue;
+            }
+          }
+        }
+      }
+      break;
+
+    case "b": //Save input, loop through and display
+      {
+        string[] user_input_array = new string[10];
+        string? user_input;
+        int string_count = 0;
+        bool running = true;
+
+        //On quit, echo from most recent -> last input. 
+        while (running)
+        {
+          Console.WriteLine("(Quit to stop)Input: ");
+          user_input = Console.ReadLine();
+
+          if (user_input?.ToLower() == "quit")
+          {
+            for (int i = 10; i > 0; i--)
+            {
+              if (string_count > 0)
+              {
+                string_count--;
+              }
+
+              Console.WriteLine($"{user_input_array[string_count]}");
+              if (string_count <= 0)
+              {
+                string_count = 10;
+              }
+            }
+            Console.ReadLine();
+            running = false;
           }
           else
           {
-            continue;
+            if (string_count == 10)
+            {
+              string_count = 0;
+            }
+            user_input_array[string_count] = user_input;
+            string_count++;
           }
         }
       }
-    }
-    break;
-
-    case "b":
-    {
-      string[] user_input_array = new string[10];
-      string? user_input;
-      int string_count = 0;
-      bool running = true;
-
-      //Sparar user input into array. On quit, echo from most recent -> last input. 
-      while (running)
-      {
-        Console.WriteLine("(Quit to stop)Input: ");
-        user_input = Console.ReadLine();
-
-        if (user_input.ToLower() == "quit")
-        {
-          for (int i = 10; i > 0; i--)
-          {
-            if (string_count > 0)
-            {
-              string_count--;
-            }
-
-            Console.WriteLine($"{user_input_array[string_count]}");
-            if (string_count <= 0)
-            {
-              string_count = 10;
-            }
-          }
-          Console.ReadLine();
-          running = false;
-        }
-        else
-        {
-          if (string_count == 10)
-          {
-            string_count = 0;
-          }
-          user_input_array[string_count] = user_input;
-          string_count++;
-        }
-      }
-    }
       break;
 
-    case "c":
+    case "c": //Adds to list, displays in quit
       {
         List<string> user_input_list = new List<string>();
         bool running = true;
@@ -130,12 +131,75 @@ while (running_main)
       }
       break;
 
+    case "d": // Read from file. If !Exists, create. 
+      {
+        if (!File.Exists(Path.Combine("Data", "Extra.CSV")))
+        {
+          if (!Directory.Exists("Data"))
+          {
+            Directory.CreateDirectory("Data");
+          }
+          File.Create(Path.Combine("Data", "Extra.CSV")).Close();
+        }
+
+        string? line;
+
+        try
+        {
+          using (StreamReader Reader = new StreamReader(Path.Combine("Data", "Extra.CSV")))
+          {
+            string? firstline = Reader.ReadLine();
+            if (firstline == null || firstline == "")
+            {
+              Reader.Close();
+              string[] LinesAdd = new string[10];
+
+              for (int i = 0; i < 10; i++)
+              {
+                LinesAdd[i] = $"This is the {(i + 1)} senctence";
+              }
+
+              using (StreamWriter Writer = new StreamWriter(Path.Combine("Data", "Extra.CSV")))
+              {
+                foreach (string LineAdd in LinesAdd)
+                {
+                  Writer.WriteLine(LineAdd);
+                }
+              }
+            }
+
+          }
+        }
+
+        catch { Console.WriteLine("File could not be read!"); }
+
+        try
+        {
+          using (StreamReader Reader = new StreamReader(Path.Combine("Data", "Extra.CSV")))
+          {
+            while ((line = Reader.ReadLine()) != null)
+            {
+              Console.WriteLine(line);
+            }
+          }
+        }
+        catch { Console.WriteLine("File could not be read!"); }
+        Console.ReadLine();
+      }
+      break;
+
     case "q":
-    {
-      running_main = false;
-    }
-    break;
+      {
+        running_main = false;
+      }
+      break;
 
 
   }
+}
+
+void ConsoleClear()
+{
+  try { Console.Clear(); }
+  catch { }
 }
